a13d682fd5e3ed546f1a68fcf877745912dfa2ef
Lukas Röllin <lsroellin@gmail.com>
2017-03-03 10:24:53 +0100

    Create Chocolatey.ps1

---
+++ Windows/Chocolatey.ps1
@@ -0,0 +1,8 @@
+# Set-ExecutionPolicy Unrestricted
+# iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+choco install keepass -y
+choco install visualstudiocode -y
+choco install googlechrome -y
+choco install spotify -y
+choco install dropbox -y
